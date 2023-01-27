11c3abe96ee89d858036b2a862c81d36d39b5586
g0dzZz1lla <g0dzZz1lla@yandex.ru>
2023-01-27 11:39:38 +0400

    Added API for calling vibrations

---
+++ Runtime/IVibrationService.cs
@@ -0,0 +1,12 @@
+﻿// Copyright © 2023 Nikolay Melnikov. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+namespace Depra.Vibration.Runtime
+{
+    public interface IVibrationService
+    {
+        void Vibrate(VibrationType type);
+
+        void CancelAll();
+    }
+}
